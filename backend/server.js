4c29026f1fd3738c47f5b123e31ccee75c448faf
Dharaneesh-Cr <25cs048@kpriet.ac.in>
2026-08-08 08:58:01 +0530

    Port update

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,9 @@ app.get("/", (req,res)=>{
      res.send("Backend is running.....");
 });
 
-const PORT=5000;
+const PORT=process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
+app.listen(PORT,"0.0.0.0",()=>{
      console.log(`Server is running on port ${PORT}`);
 });
 
